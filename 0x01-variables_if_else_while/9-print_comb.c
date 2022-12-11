103a1c1f2e80f373c8f9082fea5e59b78e7dd2ee
SaintAdoga <adogasaint@gmail.com>
2022-12-11 01:03:31 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n <= 9; n++)
 
-	{	putchar(n);
+	{	putchar((n % 10) + '0');
 
-	if (n != 57)
+		if (n == 9)
+			continue;
 	{
 		putchar(',');
 		putchar(' ');
