9e3e4606fa91ffe7ce2d3cfd47884eaaa1f67959
SaintAdoga <adogasaint@gmail.com>
2022-12-13 13:10:53 -0800

    main files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar (alp);
+
+	_putchar('\n');
+}
