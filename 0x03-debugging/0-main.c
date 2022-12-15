575a1e213ddbf20f12a818a50cfd301ff8023af3
SaintAdoga <adogasaint@gmail.com>
2022-12-14 22:54:54 -0800

    main h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * test function to print if negative or positive integer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
